69a3da6f7979ffcd21fa73e3c75f0a966cef225a
notrodans <notrodans@gmail.com>
2022-10-30 17:39:47 +0200

    update

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,7 +2,7 @@ import { Module } from "@nestjs/common"
 import { TypegooseModule } from "nestjs-typegoose"
 import { ProductController } from "./product.controller"
 import { ProductModel } from "./product.model"
-import { ProductService } from './product.service';
+import { ProductService } from "./product.service"
 
 @Module({
 	controllers: [ProductController],
